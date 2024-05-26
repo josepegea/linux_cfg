516d7a13530086d1168d214501092206b517951f
Josep Egea <jes@josepegea.com>
2024-05-26 19:57:30 +0200

    Refine closing windows and tabs for terminal

--- xkeysnail/config.py
+++ xkeysnail/config.py
@@ -41,11 +41,6 @@ define_keymap(None, {
     K("LM-BACKSLASH"): K("RM-BACKSLASH")
 }, "AltChars")
 
-define_keymap(None, {
-    K("Super-H"): sup.hide_current_app_windows,
-    K("Super-Shift-W"): sup.close_current_window
-}, "GlobalActions")
-
 define_keymap(lambda wm_class: wm_class.casefold() not in managed_apps, {
     K("Super-RIGHT"): K("END"),
     K("Super-LEFT"): K("HOME"),
@@ -126,7 +121,8 @@ define_keymap(re.compile("terminal", re.IGNORECASE),{
     K("Super-Q"): K("C-Shift-Q"),
 
     K("Super-T"): K("C-Shift-T"), # New tab
-    K("Super-Shift-W"): K("C-Shift-W"), # Close tab
+    K("Super-W"): K("C-Shift-W"), # Close tab
+    K("Super-Shift-W"): K("C-Shift-Q"), # Close window
 
     K("Super-RIGHT_BRACE"): K("C-Shift-RIGHT_BRACE"), # Zoom in
     K("Super-SLASH"): K("C-Shift-SLASH"),             # Zoom out
@@ -174,3 +170,8 @@ define_keymap(re.compile("caja", re.IGNORECASE),{
     K("Super-M-LEFT"): K("C-PAGE_UP"),    # Tab left
     K("Super-M-RIGHT"): K("C-PAGE_DOWN")  # Tab right
 }, 'Caja')
+
+define_keymap(None, {
+    K("Super-H"): sup.hide_current_app_windows,
+    K("Super-Shift-W"): sup.close_current_window
+}, "GlobalActions")
